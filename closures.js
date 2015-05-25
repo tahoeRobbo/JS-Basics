b9b62939b33b4d02572a602e7f6b38c49d0cbcb6
HoodRichRob <madmaxmacpro@mac-user.home>
2015-05-24 22:34:09 -0600

    Sun 5-24-15 9:45 -- finished exercises -- trouble getting the timed counter to work properly

--- closures.js
+++ closures.js
@@ -9,11 +9,13 @@ var outer = function(){
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+  var inner = outer();
 
 //Once you do that, invoke inner.
 
-  //Code Here
+ inner();
+
+//returns "The original name was Tyler" 
 
 
 
@@ -32,7 +34,8 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
+  var callJake = callFriend();
+	callJake("435-215-9248");
 
 
 
@@ -43,8 +46,28 @@ var callFriend = function(){
 /*
   Write a function called makeCounter that makes the following code work properly.
 */
+var makeCounter = function(addBy) {
+	if(!addBy && typeof addBy !== "undefined") {
+		var wrongInput = addBy;
+		alert("Very funny.. You can't add by " + wrongInput + ".  Try a number!");
+	} else if(addBy === 0 || addBy === undefined) {
+		var n = 0;
+		var countPlusOne = function() {
+			return (++n);
+		};
+		return countPlusOne;
+	} else {
+		var total = 0;
+		var xPlus = addBy;
+		var countPlusChoice = function() {
+			total = total + xPlus;
+			return total;
+		};
+		return countPlusChoice;
+	};
+};
+	
 
-  //Code Here
   var count = makeCounter();
   count() // 1
   count() // 2
@@ -55,14 +78,66 @@ var callFriend = function(){
 
 //Next Problem
 
+	
+	
+	/*//FE MASTERS CLOSURE W BIANCA
+	
+var add = function(num) {
+	var num1 = num;
+	var addToNum1 = function(num2) {
+		return num1 + num2;
+	};
+	return addToNum1;
+};
 
+//Counter Example FEMASTERS
 
-/*
+var counter = function() {
+	var n = 0;
+	return {
+		count: function() { return ++n; },
+		reset: function() { n = 0; }
+	};
+};
+
+*//*
   Write a function that accepts another function as it's first argument and returns a new function
   (which invokes the original function that was passed in) that can only ever be executed once.
 */
+	
+	//First non-solution
+/* var outerFn = function(x) {
+		var x = function() {
+			outerFn();
+		};
+		return x;
+	};*/
 
-  //Code Here
+
+//Try2	
+/*	
+var executeOnce = function(fnPara) {
+	var fnArg = fnPara
+	var runPassedFn = function() {
+		return fnArg();
+	};
+	return runPassedFn;
+	*/
+
+//Try 3
+
+var doOnce = function(fnPara) {
+	var fnArg = fnPara;
+	return (function(){ fnArg() })
+};
+
+var countOnce = doOnce(count);
+countOnce();
+console.log(countOnce.n);
+	
+
+
+
 
 
 
@@ -73,6 +148,14 @@ var callFriend = function(){
 /*
   Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
 */
+
+var fnCounter = function(anonFunction, n) {
+	var i = 0;
+	while (i > n) {
+		anonFunction();
+	};
+	return "STOP";
+};
 
 
 
@@ -91,30 +174,52 @@ var callFriend = function(){
 
   Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
 
-    //Answer Here
-
+//It will log out '5' five times at 5 second intervals.  I bet this based on what we saw in the lecture -- the full set of iterations will run leaving 'i' with the value '5' by the time it is ever accessed by the setTimeout method.
 
   Now, run the function in your console and note what happpens.
 
   Was your answer right or wrong?
 
-    //Answer Here
 
+//Wrong -- it logged '6' five times.. maybe because of the final 'i++' at the end of the loop? It raised the final value of 'i' to six -- makes sense because then it will not loop again ( violates i <= 5)
 
   Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc)
 */
 
-    //Code Here
 
-
+//Added IIFE **After the loop but **Before the setTimeout() method. Creates new local scope that 'locks in' all the created 'i' values.
+  var counter = function(){
+    for (var i=1; i<=5; i++) {
+			(function() {
+				setTimeout(alert( i ), (i*1000) );
+			}());
+		};
+	};
+	//CANT GET THE INTERVAL IN setTimeout TO RESPOND TO I!!!!!!!
 
 //Next Problem
 
+//TEMPLATE IIFE
+/*(function(){
+	
+}());*/
 
 
-/*
-  Make the following code work
+//  Make the following code work
+	
 
+var arrayCreator = function() {
+	return {
+		0 : function() { return 0 },
+		1 : function() { return 1 },
+		2 : function() { return 2 },
+		3 : function() { return 3 },
+		4 : function() { return 4 },
+		5 : function() { return 5 }
+	};
+};
+
+var functionArray = arrayCreator();
   funcArray[0]() //0
   funcArray[1]() //1
   funcArray[2]() //2
@@ -122,7 +227,7 @@ var callFriend = function(){
   funcArray[4]() //4
   funcArray[5]() //5
 
-  *Hint: Don't let this fool you. Break down what's really happening here.
-*/
+  //*Hint: Don't let this fool you. Break down what's really happening here.
+
 
 
--- fns-and-scope.js
+++ fns-and-scope.js
@@ -5,8 +5,13 @@ var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only argument.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
-
+ var isTyler = function(name) {
+	 if (name === "Tyler") {
+		 	return true;
+	 } else {
+	 		return false;
+	 };
+ };
 
 //Next problem
 
@@ -15,7 +20,10 @@ var name = 'Tyler';
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  //Code Here
+ var getName = function() {
+	 var userName = prompt("What's your name?");
+	 return userName;
+ };
 
 
 //Next Problem
@@ -25,17 +33,25 @@ var name = 'Tyler';
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
 
 
-//Next problem
-
+var welcome = function() {
+	var getName = function() {
+	 	var userName = prompt("What's your name?");
+	 	return userName;
+ 	};
+	alert("hello " + getName());
+};
 
 
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
+  //A parameter describes what kind of input information a function is looking for.  They are expressed in the functino definition.  ie. 
+// var name = function(first, last) where first and last are the two parameters
+
+//An argument is the information that you pass into a function to be worked on. They are expressed in the calling of the functino 
+//ie name(Robert, Hallowell) where Robert and Hallowell are the arguments, PASSED INTO THE PARAMETERS first and last.
 
 
 //Next problem
@@ -45,24 +61,40 @@ var name = 'Tyler';
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //Answer Here
+  //In javascript, these values are always falsy
+/* 
+*false
+*0
+*"" -- empty string
+null
+undefined
+NaN
+*/
 
-
-
+var falsieTest = function(x) {
+	if(!x) {
+		alert("You're Falsy!!");
+	} else {
+		alert("YO YOU DA TROOF, SON!")
+	}
+};
 //Next Problem
-
 
 
 //Create a function called myName that returns your name
 
-  //Code Here
+var myName = function() {
+		return "Robert Hallowell";
+}
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+var newMyName = myName;
+alert(newMyName());
 
 //Now alert the result of invoking newMyName
 
+//Alerts Robert Hallowell
 
 
 //Next problem
@@ -71,10 +103,21 @@ var name = 'Tyler';
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
+var outerFn = function() {
+	return function() {
+		return "Robert Hallowell";
+	}
+};
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-  //Code Here
+ var innerFn = outerFn();
+
+//console.log(innerFn) -- returns function() {
+//		return "Robert Hallowell";
+//	}
 
 //Now invoke innerFn.
+
+innerFn();
+//returns "Robert Hallowell"
